e842a38dae1e5dde9305c0c57b43e53d9ab73c40
Doan Thanh <147749764+thanhdt2145@users.noreply.github.com>
2024-11-08 13:00:30 +0700

    [HTML] - Client - Checkout

--- App/Views/Client/Pages/Checkout/Checkout.php
+++ App/Views/Client/Pages/Checkout/Checkout.php
@@ -21,86 +21,131 @@ class Checkout extends BaseView
 
         <body>
             <div class="main-container">
-                <div class="checkout-container">
-                    <form class="checkout-form">
-                        <h2 class="section-title">Thông tin liên hệ</h2>
-
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" placeholder="Nhập email của bạn">
+                <div class="checkout__container">
+                    <form class="checkout__form">
+                        <div class="checkout__form-top">
+                            <h2 class="checkout__form-title">Thông tin liên hệ</h2>
+                            <h2 class="checkout__form-login"><a href="">Đăng nhập</a></h2>
                         </div>
 
-                        <div class="checkbox-group">
+                        <div class="form-group"> 
+                        <input type="email" placeholder="Nhập email của bạn"> 
+                            
+                        </div>
+
+                        <div class="checkbox__group">
                             <input type="checkbox" id="subscribe">
                             <label for="subscribe">Gửi cho tôi tin tức và ưu đãi qua email</label>
                         </div>
 
-                        <h2 class="section-title">Giao hàng</h2>
+                        <h2 class="checkout__form-title">Giao hàng</h2>
 
                         <div class="form-group">
                             <div class="form-group">
                                 <label class="radio-label topp">
                                     <input type="radio" name="option" class="radio-input">
-                                    Tùy chọn 1
+                                    Nhận Tại Cửa Hàng
                                 </label>
                                 <label class="radio-label my-1">
                                     <input type="radio" name="option" class="radio-input">
-                                    Tùy chọn 2
+                                    Vận Chuyển
                                 </label>
                             </div>
 
                         </div>
 
                         <div class="form-group">
+    <label>Phương thức thanh toán</label>
+    <select id="paymentSelect" onchange="toggleQRCode()">
+        <option value="">Lựa Chọn</option>
+        <option value="momo">Momo</option>
+        <option value="chuyenkhoan">Chuyển khoản</option>
+        <option value="cod">Thanh toán khi nhận hàng</option>
+    </select>
+    <div class="show-image" id="show-qrcode-checkout">
+        <div class="qrcode-checkout__details">
+            <div class="qrcode-checkout__image">
+                <img src="<?= APP_URL ?>/public/assets/Client/images/payment/qrcode.png" alt="QR Code" />
+            </div>
+            <div class="qrcode-checkout__number">
+                <span>1234567889</span>
+                <div class="qrcode-checkout__namebank">
+                    <p>Tên ngân hàng: <span>VietComBank</span></p>
+                </div>
+            </div>
+            <div class="qrcode-checkout__manager">
+                <span>Thanh Doan</span>
+            </div>
+            <div class="qrcode-checkout__sub">
+                <p>
+                    Sau khi nhấp vào “Thanh toán ngay”,
+                    bạn sẽ được chuyển hướng đến OnePay -
+                    Thanh toán trực tuyến để hoàn tất việc
+                    mua hàng một cách an toàn.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+                        <div class="form-group">
                             <label>Quốc gia</label>
                             <select>
                                 <option>Việt Nam</option>
+                                <option>Lào</option>
+                                <option>Thái Lan</option>
+                                <option>Campuchia</option>
                             </select>
                         </div>
 
-                        <div class="form-group">
-                            <label>Tên</label>
-                            <input type="text" placeholder="Tên">
-                        </div>
-
-                        <div class="form-group">
-                            <label>Họ</label>
+                        <div class="form-group--name"> 
                             <input type="text" placeholder="Họ">
+                            <input type="text" placeholder="Tên">
+
                         </div>
 
                         <div class="form-group">
-                            <label>Công ty (không bắt buộc)</label>
-                            <input type="text" placeholder="Công ty">
+                            <select name="" id="">
+                                <option value="">Tỉnh thành phố</option>
+                                <option value="">Hà Nội</option>
+                                <option value="">TP HCM</option>
+                                <option value="">Cần Thơ</option>
+                                <option value="">Hậu Giang</option>
+                                <option value="">An Giang</option>
+                                <option value="">Sóc Trăng</option>
+                            </select>
                         </div>
-
                         <div class="form-group">
-                            <label>Địa chỉ</label>
+                            <select name="" id="" placeholder="">
+                                <option value="">Quận Huyện</option>
+                                <option value="">Hà Nội</option>
+                                <option value="">TP HCM</option>
+                                <option value="">Cần Thơ</option>
+                                <option value="">Hậu Giang</option>
+                                <option value="">An Giang</option>
+                                <option value="">Sóc Trăng</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
                             <input type="text" placeholder="Địa chỉ">
                         </div>
 
                         <div class="form-group">
-                            <label>Thành phố</label>
-                            <input type="text" placeholder="Thành phố">
-                        </div>
-
-                        <div class="form-group">
-                            <label>Mã bưu chính (không bắt buộc)</label>
-                            <input type="text" placeholder="Mã bưu chính">
-                        </div>
-
-                        <div class="form-group">
-                            <label>Điện thoại</label>
                             <input type="text" placeholder="Điện thoại">
                         </div>
 
-                        <div class="checkbox-group">
+                        <div class="checkbox__group">
                             <input type="checkbox" id="save-info">
                             <label for="save-info">Lưu lại thông tin</label>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class=" cta-button btn">Mua Hàng</button>
                         </div>
                     </form>
 
                     <div class="summary">
-                        <h2 class="section-title">Tóm tắt đơn hàng</h2>
+                        <h2 class="summary__title">Tóm tắt đơn hàng</h2>
 
 
                         <div class="summary__products">
@@ -108,34 +153,76 @@ class Checkout extends BaseView
                                 <div class="summary__image">
                                     <img src="<?= APP_URL ?>/public/assets/Client/images/payment/anh1.webp">
                                 </div>
-                                <div class="summary__title">Bơ Booth Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eligendi voluptatibus cumque quibusdam incidunt aliquam possimus repellat quos doloremque earum. Nobis corrupti dicta ipsam blanditiis suscipit voluptatibus aut facilis consequatur?</div>
+                                <div class="summary__details">
+                                    <div class="summary__name">Bơ Booth Lorem ipsum dolor sit amet,
+                                        consectetur adipisicing elit. Veritatis eligendi voluptatibus
+                                        cumque quibusdam incidunt aliquam possimus repellat quos
+                                        doloremque earum. Nobis corrupti dicta ipsam
+                                        blanditiis suscipit voluptatibus aut facilis consequatur?
+                                    </div>
+                                    <div class="summary__origin">
+                                        <p>Xuất sứ:<span>Úc</span> | Trọng lượng: <span>1Kg</span></p>
+                                    </div>
+                                </div>
                                 <div class="summary__price">200.000</div>
                             </div>
                             <div class="summary__item">
                                 <div class="summary__image">
                                     <img src="<?= APP_URL ?>/public/assets/Client/images/payment/anh1.webp">
                                 </div>
-                                <div class="summary__title">Bơ Booth Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eligendi voluptatibus cumque quibusdam incidunt aliquam possimus repellat quos doloremque earum. Nobis corrupti dicta ipsam blanditiis suscipit voluptatibus aut facilis consequatur?</div>
-                                <div class="summary__price">200.000</div> 
+                                <div class="summary__details">
+                                    <div class="summary__name">Bơ Booth Lorem ipsum dolor sit amet,
+                                        consectetur adipisicing elit. Veritatis eligendi voluptatibus
+                                        cumque quibusdam incidunt aliquam possimus repellat quos
+                                        doloremque earum. Nobis corrupti dicta ipsam
+                                        blanditiis suscipit voluptatibus aut facilis consequatur?
+                                    </div>
+                                    <div class="summary__origin">
+                                        <p>Xuất sứ <span>Úc</span> | Trọng lượng <span>1Kg</span></p>
+                                    </div>
+                                </div>
+                                <div class="summary__price">200.000</div>
                             </div>
                             <div class="summary__voucher">
-                                    <input type="text">
-                                    <button class="btn ">Áp Dụng</button>
+                                <input type="text">
+                                <a type="submit" href="#" class="btn ">Áp Dụng</a href="">
+                            </div>
+
+                            <div class="summary__total-products">
+                                <span class="summary__total-text">
+                                    Tổng đơn hàng: 3 mặt hàng
+                                </span>
+                                <span class="summary__total-price">6.725.600 </span>
+                            </div>
+                            <div class="summary__delivery">
+                                <span class="summary__method">Nhận tại cửa hàng</span>
+                                <span class="summary__delivery-fee">Miễn Phí</span>
+                            </div>
+                            <div class="summary__total--after-discount">
+                                <span class="summary__total-text ">Tổng tiền</span>
+                                <div class="summary__total-price ">
+                                    <p><span>6.725.600</span>VNĐ</p>
                                 </div>
 
-                                <div class="summary-item">
-                                    <span>Vận chuyển</span>
-                                    <span>MIỄN PHÍ</span>
-                                </div>
-
-                                <div class="summary-item total">
-                                    <span>Tổng</span>
-                                    <span>3.340.000 đ</span>
-                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <script >
+                // Ẩn tất cả các phần chi tiết thanh toán
+                function toggleQRCode() {
+                const paymentMethod = document.getElementById("paymentSelect").value;
+                const qrCodeSection = document.getElementById("show-qrcode-checkout");
+
+                if (paymentMethod == "chuyenkhoan") {
+                    qrCodeSection.style.display = "block"; // Hiển thị mã QR
+                } else {
+                    qrCodeSection.style.display = "none"; // Ẩn mã QR
+                }
+}
+
+            </script>
         </body>
 
         </html>
